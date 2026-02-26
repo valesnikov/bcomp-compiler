57cb7f2d91668456236f0b29c367b6a4450835b4
Vladimir Lesnikov <v.a.lesnikov@vk.com>
2026-02-26 15:32:58 +0300

    fix unsafe optimizations and int16 folding

--- app/Optimize.hs
+++ app/Optimize.hs
@@ -2,6 +2,7 @@ module Optimize (preEvaluate, postOptimize) where
 
 import Bcomp (Addr (AddrAbs, AddrFwd), BcompAsm, Op (..))
 import Data.Bits (Bits (complement, (.&.), (.|.)))
+import Data.Int (Int16)
 import Defs (Expr (..), LogicExpr (..), Stmt (..))
 
 preEvaluate :: Stmt -> Stmt
@@ -19,7 +20,10 @@ evalStmt x = case x of
   SIf LFalse _ Nothing -> SBlock []
   SIf LFalse _ (Just elseB) -> elseB
   SIf LTrue ifB _ -> ifB
-  SIf _ (SBlock []) Nothing -> SBlock []
+  SIf cond (SBlock []) Nothing ->
+    if isPureLexpr cond
+      then SBlock []
+      else SIf (evalLexpr cond) (SBlock []) Nothing
   SIf cond ifB elseB ->
     SIf (evalLexpr cond) (evalStmt ifB) (evalStmt <$> elseB)
   SWhile LFalse _ -> SBlock []
@@ -46,22 +50,31 @@ evalExpr = exprOpt . f
       (EConst val) -> EConst val
       (EIdent val) -> EIdent val
       (ELoad val) -> ELoad val
-      (EOpNeg (EConst n)) -> EConst (-n)
-      (EOpAsl (EConst n)) -> EConst (n * 2)
-      (EOpAsr (EConst n)) -> EConst (n `div` 2)
-      (EOpNot (EConst n)) -> EConst (complement n)
-      (EOpAdd (EConst n) (EConst m)) -> EConst (m + n)
-      (EOpSub (EConst n) (EConst m)) -> EConst (m - n)
-      (EOpAnd (EConst n) (EConst m)) -> EConst (m .&. n)
-      (EOpOr (EConst n) (EConst m)) -> EConst (m .|. n)
+      (EOpNeg (EConst n)) -> EConst $ i16Neg n
+      (EOpAsl (EConst n)) -> EConst $ i16Asl n
+      (EOpAsr (EConst n)) -> EConst $ i16Asr n
+      (EOpNot (EConst n)) -> EConst $ i16Not n
+      (EOpAdd (EConst n) (EConst m)) -> EConst $ i16Add n m
+      (EOpSub (EConst n) (EConst m)) -> EConst $ i16Sub n m
+      (EOpAnd (EConst n) (EConst m)) -> EConst $ i16And n m
+      (EOpOr (EConst n) (EConst m)) -> EConst $ i16Or n m
       (EOpNeg val) -> EOpNeg $ f val
       (EOpAsl val) -> EOpAsl $ f val
       (EOpAsr val) -> EOpAsr $ f val
       (EOpNot val) -> EOpNot $ f val
       (EOpAdd a b) -> EOpAdd (f a) (f b)
-      (EOpSub a b) -> if a == b then EConst 0 else EOpSub (f a) (f b)
-      (EOpAnd a b) -> if a == b then a else EOpAnd (f a) (f b)
-      (EOpOr a b) -> if a == b then a else EOpOr (f a) (f b)
+      (EOpSub a b) ->
+        let a' = f a
+            b' = f b
+         in if isPureExpr a' && a' == b' then EConst 0 else EOpSub a' b'
+      (EOpAnd a b) ->
+        let a' = f a
+            b' = f b
+         in if isPureExpr a' && a' == b' then a' else EOpAnd a' b'
+      (EOpOr a b) ->
+        let a' = f a
+            b' = f b
+         in if isPureExpr a' && a' == b' then a' else EOpOr a' b'
       (ECall n es) -> ECall n (map f es)
       (EIn _) -> x
 
@@ -69,23 +82,62 @@ evalLexpr :: LogicExpr -> LogicExpr
 evalLexpr x = case x of
   LTrue -> LTrue
   LFalse -> LFalse
-  LOpEq (EConst n) (EConst m) -> if n == m then LTrue else LFalse
-  LOpEq v1 v2 -> if v1 == v2 then LTrue else LOpEq (evalExpr v1) (evalExpr v2)
-  LOpNeq (EConst n) (EConst m) -> if n /= m then LTrue else LFalse
-  LOpNeq v1 v2 -> if v1 == v2 then LFalse else LOpNeq (evalExpr v1) (evalExpr v2)
-  LOpLt (EConst n) (EConst m) -> if n < m then LTrue else LFalse
-  LOpLt v1 v2 -> if v1 == v2 then LFalse else LOpLt (evalExpr v1) (evalExpr v2)
-  LOpGt (EConst n) (EConst m) -> if n > m then LTrue else LFalse
-  LOpGt v1 v2 -> if v1 == v2 then LFalse else LOpGt (evalExpr v1) (evalExpr v2)
-  LOpLe (EConst n) (EConst m) -> if n <= m then LTrue else LFalse
-  LOpLe v1 v2 -> if v1 == v2 then LTrue else LOpLe (evalExpr v1) (evalExpr v2)
-  LOpGe (EConst n) (EConst m) -> if n >= m then LTrue else LFalse
-  LOpGe v1 v2 -> if v1 == v2 then LTrue else LOpGe (evalExpr v1) (evalExpr v2)
+  LOpEq v1 v2 ->
+    let a = evalExpr v1
+        b = evalExpr v2
+     in case (a, b) of
+          (EConst n, EConst m) -> boolToLexpr $ i16Cmp (==) n m
+          _ -> if isPureExpr a && a == b then LTrue else LOpEq a b
+  LOpNeq v1 v2 ->
+    let a = evalExpr v1
+        b = evalExpr v2
+     in case (a, b) of
+          (EConst n, EConst m) -> boolToLexpr $ i16Cmp (/=) n m
+          _ -> if isPureExpr a && a == b then LFalse else LOpNeq a b
+  LOpLt v1 v2 ->
+    let a = evalExpr v1
+        b = evalExpr v2
+     in case (a, b) of
+          (EConst n, EConst m) -> boolToLexpr $ i16Cmp (<) n m
+          _ -> if isPureExpr a && a == b then LFalse else LOpLt a b
+  LOpGt v1 v2 ->
+    let a = evalExpr v1
+        b = evalExpr v2
+     in case (a, b) of
+          (EConst n, EConst m) -> boolToLexpr $ i16Cmp (>) n m
+          _ -> if isPureExpr a && a == b then LFalse else LOpGt a b
+  LOpLe v1 v2 ->
+    let a = evalExpr v1
+        b = evalExpr v2
+     in case (a, b) of
+          (EConst n, EConst m) -> boolToLexpr $ i16Cmp (<=) n m
+          _ -> if isPureExpr a && a == b then LTrue else LOpLe a b
+  LOpGe v1 v2 ->
+    let a = evalExpr v1
+        b = evalExpr v2
+     in case (a, b) of
+          (EConst n, EConst m) -> boolToLexpr $ i16Cmp (>=) n m
+          _ -> if isPureExpr a && a == b then LTrue else LOpGe a b
 
 exprOpt :: Expr -> Expr
-exprOpt = f
+exprOpt = go
   where
-    f ex = case ex of
+    go ex
+      | isPureExpr ex = pureExprOpt ex
+      | otherwise = case ex of
+          (EOpNeg x) -> EOpNeg $ go x
+          (EOpAsl x) -> EOpAsl $ go x
+          (EOpAsr x) -> EOpAsr $ go x
+          (EOpNot x) -> EOpNot $ go x
+          (EOpAdd x1 x2) -> EOpAdd (go x1) (go x2)
+          (EOpSub x1 x2) -> EOpSub (go x1) (go x2)
+          (EOpAnd x1 x2) -> EOpAnd (go x1) (go x2)
+          (EOpOr x1 x2) -> EOpOr (go x1) (go x2)
+          (ELoad x) -> ELoad $ go x
+          (ECall n es) -> ECall n (map go es)
+          _ -> ex
+
+    pureExprOpt ex = case ex of
       (EOpNeg (EOpNeg a)) -> a -- -(-a) -> a
       (EOpNeg (EOpSub a b)) -> EOpSub b a -- -(a-b) -> b-a
       --
@@ -107,19 +159,85 @@ exprOpt = f
       (EOpSub (EOpAdd a b) (EOpSub c d)) -> EOpAdd (EOpSub (EOpAdd a b) c) d -- (a+b)-(c-d) -> ((a+b)-c)+d
       (EOpSub (EOpSub a b) (EOpSub c d)) -> EOpAdd (EOpSub (EOpSub a b) c) d -- (a-b)-(c-d) -> ((a-b)-c)+d
       --
-      (EOpNeg x) -> EOpNeg $ f x
-      (EOpAsl x) -> EOpAsl $ f x
-      (EOpAsr x) -> EOpAsr $ f x
-      (EOpNot x) -> EOpNot $ f x
-      (EOpAdd x1 x2) -> EOpAdd (f x1) (f x2)
-      (EOpSub x1 x2) -> EOpSub (f x1) (f x2)
-      (EOpAnd x1 x2) -> EOpAnd (f x1) (f x2)
-      (EOpOr x1 x2) -> EOpOr (f x1) (f x2)
+      (EOpNeg x) -> EOpNeg $ go x
+      (EOpAsl x) -> EOpAsl $ go x
+      (EOpAsr x) -> EOpAsr $ go x
+      (EOpNot x) -> EOpNot $ go x
+      (EOpAdd x1 x2) -> EOpAdd (go x1) (go x2)
+      (EOpSub x1 x2) -> EOpSub (go x1) (go x2)
+      (EOpAnd x1 x2) -> EOpAnd (go x1) (go x2)
+      (EOpOr x1 x2) -> EOpOr (go x1) (go x2)
       (EConst _) -> ex
       (EIdent _) -> ex
-      (ELoad _) -> ex
-      (ECall n es) -> ECall n (map f es)
+      (ELoad x) -> ELoad $ go x
+      (ECall n es) -> ECall n (map go es)
       (EIn _) -> ex
+
+isPureExpr :: Expr -> Bool
+isPureExpr expr = case expr of
+  EConst _ -> True
+  EIdent _ -> True
+  ELoad x -> isPureExpr x
+  EOpNeg x -> isPureExpr x
+  EOpAsl x -> isPureExpr x
+  EOpAsr x -> isPureExpr x
+  EOpNot x -> isPureExpr x
+  EOpAdd x1 x2 -> isPureExpr x1 && isPureExpr x2
+  EOpSub x1 x2 -> isPureExpr x1 && isPureExpr x2
+  EOpAnd x1 x2 -> isPureExpr x1 && isPureExpr x2
+  EOpOr x1 x2 -> isPureExpr x1 && isPureExpr x2
+  ECall _ _ -> False
+  EIn _ -> False
+
+isPureLexpr :: LogicExpr -> Bool
+isPureLexpr lexpr = case lexpr of
+  LTrue -> True
+  LFalse -> True
+  LOpEq x1 x2 -> isPureExpr x1 && isPureExpr x2
+  LOpNeq x1 x2 -> isPureExpr x1 && isPureExpr x2
+  LOpLt x1 x2 -> isPureExpr x1 && isPureExpr x2
+  LOpGt x1 x2 -> isPureExpr x1 && isPureExpr x2
+  LOpLe x1 x2 -> isPureExpr x1 && isPureExpr x2
+  LOpGe x1 x2 -> isPureExpr x1 && isPureExpr x2
+
+toI16 :: Integer -> Int16
+toI16 = fromInteger
+
+fromI16 :: Int16 -> Integer
+fromI16 = toInteger
+
+i16Neg :: Integer -> Integer
+i16Neg n = fromI16 $ negate $ toI16 n
+
+i16Asl :: Integer -> Integer
+i16Asl n = fromI16 $ toI16 n * 2
+
+i16Asr :: Integer -> Integer
+i16Asr n = fromI16 $ toI16 n `div` 2
+
+i16Not :: Integer -> Integer
+i16Not n = fromI16 $ complement $ toI16 n
+
+i16Add :: Integer -> Integer -> Integer
+i16Add n m = fromI16 $ toI16 n + toI16 m
+
+i16Sub :: Integer -> Integer -> Integer
+i16Sub n m = fromI16 $ toI16 n - toI16 m
+
+i16And :: Integer -> Integer -> Integer
+i16And n m = fromI16 $ toI16 n .&. toI16 m
+
+i16Or :: Integer -> Integer -> Integer
+i16Or n m = fromI16 $ toI16 n .|. toI16 m
+
+i16Cmp :: (Int16 -> Int16 -> Bool) -> Integer -> Integer -> Bool
+i16Cmp cmp n m = cmp (toI16 n) (toI16 m)
+
+boolToLexpr :: Bool -> LogicExpr
+boolToLexpr b =
+  if b
+    then LTrue
+    else LFalse
 
 postOptimize :: BcompAsm -> BcompAsm
 postOptimize = unusedLabelsOpt . singlePassOpt . afterJumpOpt
