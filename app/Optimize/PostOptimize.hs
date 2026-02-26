1619b33bd2287790f29403cf6608399f9ed879b2
Vladimir Lesnikov <v.a.lesnikov@vk.com>
2026-02-26 16:23:50 +0300

    optimize labels

--- app/Optimize/PostOptimize.hs
+++ app/Optimize/PostOptimize.hs
@@ -1,12 +1,94 @@
 module Optimize.PostOptimize (postOptimize) where
 
-import Bcomp (Addr (AddrAbs, AddrFwd), BcompAsm, Op (..))
+import Bcomp (Addr (..), BcompAsm, Op (..))
+import qualified Data.Map.Strict as Map
+import qualified Data.Set as Set
 
 postOptimize :: BcompAsm -> BcompAsm
-postOptimize = unusedLabelsOpt . singlePassOpt . afterJumpOpt
+postOptimize = unusedLabelsOpt . mergeAdjacentLabelsOpt . singlePassOpt . afterJumpOpt
+
+mergeAdjacentLabelsOpt :: BcompAsm -> BcompAsm
+mergeAdjacentLabelsOpt ops = map rewriteOp mergedOps
+  where
+    (mergedOps, aliases) = go ops Nothing [] Map.empty
+
+    go [] _ acc aliasMap = (reverse acc, aliasMap)
+    go (OP_LABEL l : xs) Nothing acc aliasMap = go xs (Just l) (OP_LABEL l : acc) aliasMap
+    go (OP_LABEL l : xs) (Just root) acc aliasMap = go xs (Just root) acc (Map.insert l root aliasMap)
+    go (x : xs) _ acc aliasMap = go xs Nothing (x : acc) aliasMap
+
+    canonical l = case Map.lookup l aliases of
+      Nothing -> l
+      Just l' ->
+        if l' == l
+          then l
+          else canonical l'
+
+    rewriteAddr (AddrAbs l) = AddrAbs (canonical l)
+    rewriteAddr (AddrRel l) = AddrRel (canonical l)
+    rewriteAddr (AddrInd l) = AddrInd (canonical l)
+    rewriteAddr (AddrIndI l) = AddrIndI (canonical l)
+    rewriteAddr (AddrIndD l) = AddrIndD (canonical l)
+    rewriteAddr a = a
+
+    rewriteOp (OP_AND a) = OP_AND (rewriteAddr a)
+    rewriteOp (OP_OR a) = OP_OR (rewriteAddr a)
+    rewriteOp (OP_ADD a) = OP_ADD (rewriteAddr a)
+    rewriteOp (OP_ADC a) = OP_ADC (rewriteAddr a)
+    rewriteOp (OP_SUB a) = OP_SUB (rewriteAddr a)
+    rewriteOp (OP_CMP a) = OP_CMP (rewriteAddr a)
+    rewriteOp (OP_LOOP a) = OP_LOOP (rewriteAddr a)
+    rewriteOp (OP_LD a) = OP_LD (rewriteAddr a)
+    rewriteOp (OP_SWAM a) = OP_SWAM (rewriteAddr a)
+    rewriteOp (OP_JUMP a) = OP_JUMP (rewriteAddr a)
+    rewriteOp (OP_CALL a) = OP_CALL (rewriteAddr a)
+    rewriteOp (OP_ST a) = OP_ST (rewriteAddr a)
+    rewriteOp (OP_BEQ l) = OP_BEQ (canonical l)
+    rewriteOp (OP_BNE l) = OP_BNE (canonical l)
+    rewriteOp (OP_BMI l) = OP_BMI (canonical l)
+    rewriteOp (OP_BPL l) = OP_BPL (canonical l)
+    rewriteOp (OP_BHIS l) = OP_BHIS (canonical l)
+    rewriteOp (OP_BLO l) = OP_BLO (canonical l)
+    rewriteOp (OP_BVS l) = OP_BVS (canonical l)
+    rewriteOp (OP_BVC l) = OP_BVC (canonical l)
+    rewriteOp (OP_BLT l) = OP_BLT (canonical l)
+    rewriteOp (OP_BGE l) = OP_BGE (canonical l)
+    rewriteOp op = op
 
 unusedLabelsOpt :: BcompAsm -> BcompAsm
-unusedLabelsOpt = id -- TODO
+unusedLabelsOpt ops = filter keep ops
+  where
+    usedLabels = foldl collect Set.empty ops
+
+    keep (OP_LABEL l) = l == "START" || l `Set.member` usedLabels
+    keep _ = True
+
+    collect acc (OP_AND a) = collectAddr acc a
+    collect acc (OP_OR a) = collectAddr acc a
+    collect acc (OP_ADD a) = collectAddr acc a
+    collect acc (OP_ADC a) = collectAddr acc a
+    collect acc (OP_SUB a) = collectAddr acc a
+    collect acc (OP_CMP a) = collectAddr acc a
+    collect acc (OP_LOOP a) = collectAddr acc a
+    collect acc (OP_LD a) = collectAddr acc a
+    collect acc (OP_SWAM a) = collectAddr acc a
+    collect acc (OP_JUMP a) = collectAddr acc a
+    collect acc (OP_CALL a) = collectAddr acc a
+    collect acc (OP_ST a) = collectAddr acc a
+    collect acc (OP_BEQ l) = Set.insert l acc
+    collect acc (OP_BNE l) = Set.insert l acc
+    collect acc (OP_BMI l) = Set.insert l acc
+    collect acc (OP_BPL l) = Set.insert l acc
+    collect acc (OP_BHIS l) = Set.insert l acc
+    collect acc (OP_BLO l) = Set.insert l acc
+    collect acc (OP_BVS l) = Set.insert l acc
+    collect acc (OP_BVC l) = Set.insert l acc
+    collect acc (OP_BLT l) = Set.insert l acc
+    collect acc (OP_BGE l) = Set.insert l acc
+    collect acc _ = acc
+
+    collectAddr acc (AddrAbs l) = Set.insert l acc
+    collectAddr acc _ = acc
 
 afterJumpOpt :: BcompAsm -> BcompAsm
 afterJumpOpt = removeBetweenJumps
